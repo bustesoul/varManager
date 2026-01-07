95655bf6e054f05246a61a437470f93453b95acd
bustesoul <32890006+bustesoul@users.noreply.github.com>
2026-01-08 01:30:53 +0800

    bug fix

--- varManager/Models/ScenesView.cs
+++ varManager/Models/ScenesView.cs
@@ -12,10 +12,13 @@ namespace varManager.Models
         public string? CreatorName { get; set; }
         public string? PackageName { get; set; }
         public DateTime? MetaDate { get; set; }
+        public DateTime? VarDate { get; set; }
         public string? Version { get; set; }
         public bool Installed { get; set; }
         public bool Disabled { get; set; }
         public bool Hide { get; set; }
         public bool Fav { get; set; }
+        public int HideFav { get; set; }
+        public string? Location { get; set; }
     }
 }
